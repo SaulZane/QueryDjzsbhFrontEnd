acd93724309645d21f2bb75a6ff28e31b7ca402b
Saul Zane <896841013@qq.com>
2024-08-08 00:07:46 +0800

    初始化配置

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+ <var-button type="primary">主要按钮</var-button>
+ <var-button>默认按钮</var-button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Varlet from '@varlet/ui'
+import '@varlet/ui/es/style'
+
+createApp(App).use(Varlet).mount('#app')
